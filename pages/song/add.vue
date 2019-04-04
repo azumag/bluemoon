4f5ab0623f3cc0e097c705ee931bf2e4d732402a
kyousuke maruyama <tempakyousuke@gmail.com>
2019-04-04 20:44:27 +0900

    prepare song name property

--- pages/song/add.vue
+++ pages/song/add.vue
@@ -4,7 +4,7 @@ v-layout
     v-card
       v-card-title.headline 楽曲を追加
       v-card-text
-        v-text-field(label='曲名')
+        v-text-field(v-model='songName' label='曲名')
       v-card-actions
         v-spacer
         v-btn(@click='addSong') 登録
@@ -14,6 +14,8 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class AddSong extends Vue {
+  songName: string = ''
+
   addSong() {}
 }
 </script>
