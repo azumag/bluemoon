bf8917215e02100612334649dcab6b31ff8462cf
kyousuke maruyama <tempakyousuke@gmail.com>
2019-04-04 20:34:13 +0900

    creat add song component

---
+++ pages/song/add.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+v-layout
+  v-flex(text-xs-center)
+    v-card
+      v-card-title.headline 楽曲を追加
+      v-card-text
+        v-text-field(label='曲名')
+      v-card-actions
+        v-spacer
+        v-btn 登録
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AddSong extends Vue {}
+</script>
