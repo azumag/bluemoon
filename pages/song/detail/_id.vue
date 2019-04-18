8271a39621fb6cbcea10e75e50a20be66ae9507b
kyousuke maruyama <tempakyousuke@gmail.com>
2019-04-18 19:44:50 +0900

    詳細用Componentを用意

---
+++ pages/song/detail/_id.vue
@@ -0,0 +1,15 @@
+
+<template lang="pug">
+v-layout
+  v-flex(text-xs-center)
+    v-card
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import firebase from '@/plugins/firebase'
+
+const db = firebase.firestore()
+
+@Component
+export default class SongDetail extends Vue {}
+</script>
