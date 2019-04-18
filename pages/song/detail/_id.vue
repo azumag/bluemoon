4f2888c658041ed4c5f38ed3973ac2f977d97922
kyousuke maruyama <tempakyousuke@gmail.com>
2019-04-18 19:55:59 +0900

    楽曲情報を取得

--- pages/song/detail/_id.vue
+++ pages/song/detail/_id.vue
@@ -11,5 +11,23 @@ import firebase from '@/plugins/firebase'
 const db = firebase.firestore()
 
 @Component
-export default class SongDetail extends Vue {}
+export default class SongDetail extends Vue {
+  song: {
+    name: string
+  } = {}
+
+  getSong() {
+    const id: string = this.$route.params.id
+    const songRef = db.collection('songs').doc(id)
+    songRef.get().then(doc => {
+      this.song = {
+        name: doc.data().name
+      }
+    })
+  }
+
+  created() {
+    this.getSong()
+  }
+}
 </script>
