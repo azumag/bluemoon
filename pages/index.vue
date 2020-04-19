59a70200164de5d656ece8ebab8e6445d79384bc
azumag <tsubasa.azumagakito@gmail.com>
2020-04-19 23:10:48 +0900

    update top

--- pages/index.vue
+++ pages/index.vue
@@ -24,56 +24,21 @@ v-layout(column, justify-center, align-center)
             text-color="white"
           )
             | お知らせ
-          v-img(
-            class="white--text align-end"
-            height="100px"
-            src="/fes2.jpg"
+          v-card-title.title.white--text(
+            style="background-image: url('/fes2.jpg'); background-color: rgba(80, 80, 80, 0.4)"
             @click.stop="$router.push('/events/')"
           )
-            v-card-title.title(
-              style="background-color: rgba(80, 80, 80, 0.4)"
-            )
-              | Bluegrass Lockdown Music Festival エントリー募集中！
-              //- v-btn.ml-auto(
-              //-   @click.stop="$router.push('/events/')"
-              //-   color='red'
-              //-   class="float-right ma-2"
-              //- )
-                | 詳細
+            | Bluegrass Lockdown Music Festival エントリー募集中！
           a(href="https://www.facebook.com/RockyTopTokyo/posts/2670280893228677?__xts__[0]=68.ARDHkjV9GOVE-nfhPgXgKlz8cCzsjj9_ao3UpNwn8Yz-deQvfoAcFGKrX9oF4dvGYxTbLMC2KHB9_--6gxKNpYq0eRIegA8BQHkS0cgTfu37AaTIGqmEDfkHhdzJVk95DyU-TQsBFeliYSrwkiD865FtFBCQGAQo6aCv_veb1ilMpSMV1FjCqjmkDon9hTBVspWofxkxEHwvrT-xpOhj28NttSlibjlGrY0DKYZxel2pUSQw4vjJSVAZz6fIRufQ9wapOFWYR-WERX-hGxMBpVUCJbIFc8N2Y1KlVUrJY5iVal9HmDMjaRlczZrm7RSGOs9FkHheq9uLfgGrptilDqFy&__tn__=-R")
-            v-img(
-              class="white--text align-end"
-              height="100px"
-              src="/events2.jpg"
+            v-card-title.title.white--text(
+              style="background-image: url('/events2.jpg'); background-color: rgba(100, 100, 100, 0.4)"
             )
-              v-card-title.title(
-                style="background-color: rgba(100, 100, 100, 0.4)"
-              )
-                | 銀座ロッキートップ存続のための支援のお願い
-              //- v-btn.ml-auto(
-              //-   class="ma-2"
-              //-   color="red"
-              //-   text-color="white"
-              //-   href="https://www.facebook.com/RockyTopTokyo/posts/2670280893228677?__xts__[0]=68.ARDHkjV9GOVE-nfhPgXgKlz8cCzsjj9_ao3UpNwn8Yz-deQvfoAcFGKrX9oF4dvGYxTbLMC2KHB9_--6gxKNpYq0eRIegA8BQHkS0cgTfu37AaTIGqmEDfkHhdzJVk95DyU-TQsBFeliYSrwkiD865FtFBCQGAQo6aCv_veb1ilMpSMV1FjCqjmkDon9hTBVspWofxkxEHwvrT-xpOhj28NttSlibjlGrY0DKYZxel2pUSQw4vjJSVAZz6fIRufQ9wapOFWYR-WERX-hGxMBpVUCJbIFc8N2Y1KlVUrJY5iVal9HmDMjaRlczZrm7RSGOs9FkHheq9uLfgGrptilDqFy&__tn__=-R"
-              //- )
-                | 詳細
+              | 銀座ロッキートップ存続のための支援のお願い
           a(href="https://camp-fire.jp/projects/view/254796")
-            v-img(
-              class="white--text align-end"
-              height="100px"
-              src="/fes3.jpg"
+            v-card-title.title.white--text(
+              style="background-image: url('fes3.jpg'); background-color: rgba(100, 100, 100, 0.1)"
             )
-              v-card-title.title(
-                style="background-color: rgba(100, 100, 100, 0.6)"
-              )
-                | 打倒コロナ！本郷「アルマジロ」から届け！
-              //- v-btn.ml-auto(
-              //-   class="ma-2"
-              //-   color="red"
-              //-   text-color="white"
-              //-   href="https://camp-fire.jp/projects/view/254796"
-              //- )
-                | 詳細
+              | 打倒コロナ！本郷「アルマジロ」から届け！
       v-col(cols=12)
         v-card(color='rgb(100, 100, 100, 0.4)' shaped)
           v-card-title.headline
