7c4d020aad1c8124486a5ea7dadf0bc8e14f2b1a
azumag <tsubasa.azumagakito@gmail.com>
2020-12-15 15:41:53 +0900

    update timetable

--- pages/events/_id/index.vue
+++ pages/events/_id/index.vue
@@ -128,68 +128,216 @@ v-layout(column, justify-center, align-center)
                 | * 不備・漏れなどがありましたらお申し付けください →
                 a(href='https://twitter.com/bluegrass_moon' target='_blank')
                   | @bluegrass_moon
-              v-simple-table
-                template
-                  //- thead
-                  //-   tr
-                  //-     th test
-                  //-     th b
-                  tbody
-                    tr
-                      td 17:30
-                      td 
-                        a(href="https://cluster.mu/" target='_blank') Cluster 
-                        | フェスバーチャル会場の開場 （入退場自由）
-                    tr
-                      td 18:15 - 
-                      td Youtube 放送開始
-                    tr
-                      td 18:15 - 19:30
-                      td
-                        div.ma-4 Lingerie Douglas
-                        div.ma-4 BLUEST BELIEVERS
-                        div.ma-4 もっつ
-                        div.ma-4 まるみ
-                        div.ma-4 A-RISE
-                        div.ma-4 自由人 :-)
-                        div.ma-4 Hiroshi Masuda
-                        div.ma-4 Two Crie Stringband
-                    tr
-                      td 19:30 - 21:00
-                      td
-                        div.ma-4 Lonesome Garage Band
-                        div.ma-4 もひかん
-                        div.ma-4 Masaoka and Friends Band
-                        div.ma-4 引きこもりジジイ
-                        div.ma-4 扇町ボンバーズ
-                        div.ma-4 米屋 with 原宿限界タピオカボーイズ
-                        div.ma-4 ゴールデンロッドガールズ
-                        div.ma-4 ゆうと
-                    tr
-                      td 21:00 - 21:15
-                      td 中入り（休憩） Cluster バーチャルイベント会場切り替えのため
-                    tr
-                      td 21:15 - 22:30
-                      td 
-                        div.ma-4 Unripened Stringplunkers
-                        div.ma-4 B-RISE
-                        div.ma-4 日本アルプス弦楽団
-                        div.ma-4 箸休め?Sacred
-                        div.ma-4 Copper Kettles
-                        div.ma-4 まいばす
-                        div.ma-4 れいじーぴっかーず
-                    tr
-                      td 22:30 - 24:00
-                      td 
-                        div.ma-4 鳩正宗
-                        div.ma-4 雑貨屋フレイヴァー
-                        div.ma-4 マーマレードボーイズ
-                        div.ma-4 コーラスウォーター
-                        div.ma-4 MTB
-                        div.ma-4 Deep Blue Boys
-                        div.ma-4 ジャンクション
-                        div.ma-4 かみおか（おだ）
-                        div.ma-4 水木ゆうこ＆サウザンドリバー
+              v-timeline
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 17:30
+                    v-card-text
+                      a(href="https://cluster.mu/" target='_blank') Cluster 
+                      | フェスバーチャル会場の開場 （入退場自由）
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 18:15
+                    v-card-text
+                      | Youtube 放送開始
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 18:15 - 19:30
+                    v-card-text
+                      v-expansion-panels
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Lingerie Douglas
+                          v-expansion-panel-content
+                            | 本人は多分覚えて無いとはおもいますがドブロの巨匠Jerry Douglas氏からランジェリー ダグラスを名乗っても良いとのお許しを頂いているので今回はランジェリー ・ダグラスでエントリーします。 曲はオリジナルの”Island Christmas “とFlatt & Scruggsのドブロ弾きJosh Gravesの”Foggy mountain rock”です。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | BLUEST BELIEVERS
+                          v-expansion-panel-content
+                            | 2019年夏、東北大のブルーグラス信者たちによって結成されたフェスバンドです。 二十歳からアラフォーまで幅広い年齢層でお送りします☆ お前ら...心に宿った青い炎が消えないかぎり...信じることを...やめるんじゃねぇぞ...！ Don’t stop believing！！！
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | もっつ
+                          v-expansion-panel-content
+                            | エセfoggy mountain breakdown(flatt&scruggsの初期バージョン)
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | まるみ
+                          v-expansion-panel-content
+                            | ブルーグラスを始めてもう40年になってしまいました。典型的な下手の横好きですが、好きなので、体が動く限り続けたいと思います。Long Black Veilをソロで演奏させていただきました。トラディショナル曲のように聞こえますが、1950年代のレフティ・フリーゼルのカントリーヒット曲。ブルーグラス系はもちろん、The Bandはじめ多くのアーティストがジャンルを超えて取り上げています。珍しいところではミック・ジャガーも。Webまるみというサイトをやっています。ブルーグラスフェスが大好きで、がんばってフェススケジュールをフォローしています。今年はとても残念な年でしたが、来年はぜひ、フェス会場で皆様とお会いできますように！
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | A-RISE
+                          v-expansion-panel-content
+                            | 調布で活躍する味のあるバンドです。リーダー アライの魂のこもった歌詞に惚れてください。下北沢「らうん」にてたまにライブしています。今回はその「らうん」での演奏を2曲，エントリーします。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 自由人 :-)
+                          v-expansion-panel-content
+                            | 2011年、音楽性の違いにより結成。個々の単独思想から生まれる自由な音楽がウリ。名大OBバンドとして細く長く活動しているが、その存在は広く知られてはいない。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Hiroshi Masuda
+                          v-expansion-panel-content
+                            | 還暦バンジョー弾き、増田と申します。クリスマスナンバー1曲「Sleigh Ride」と、還暦ギタリスト高橋新吾とのユニット「マスダスインゴ」によるBeatles Medleyをエントリーさせて下さい。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Two Crie Stringband
+                          v-expansion-panel-content
+                            | トゥークリエストリングバンドと読みます。実はバンドじゃなくてデュオです。 オールドタイムやフォークを中心にお届けします！ 今回はバンドで演奏できました！やったー！！ オールドタイムに興味を持った方はこちらのTwitterアカウントのフォローをぜひお願いします🙇‍♀️ → @OldTimeJP
+                       
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 19:30 - 21:00
+                    v-card-text
+                      v-expansion-panels
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Lonesome Garage Band
+                          v-expansion-panel-content
+                            | Where is my destination?
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | もひかん
+                          v-expansion-panel-content
+                            | 前回の楽しそうなご様子や、SNSで繋がる皆さんに背中を押して頂き14年振りにまたドブロを触ることになりました。 相当なブランクでお耳汚しとは思いますが、40の手習いを優しく見守って頂けたら幸いです。 曲は賛美歌のhow great thou art （というかロブアイクスのコピー）です。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Masaoka and Friends Band
+                          v-expansion-panel-content
+                            | 本当にごめんなさい、今回は大学の修士論文の中間発表などもあり忙しく、 皆さんが待ち望んでいた茶番タイムがありません…… でも参加はしたかったので時間を探して動画を撮りました。良ければ聞いて下さい。 締め切り日の朝に動画内でバンジョーの名前入れ忘れてることに気づいたんですけど普通に正岡が弾いてます。 動画内のあれは、実在のあれとなんも関係ないです。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 引きこもりジジイ
+                          v-expansion-panel-content
+                            | 63歳で、一からアパラチアン・ダルシマーを学び直した、来年70歳になるジジイです。 アパラチアン・ダルシマーを、フラット・ピッキングとフィンガー・ピッキングで弾きます。 ドライヴのかかった、かっこいいブルーグラスの合間に、もたもた弾きます。休憩時間としてお使いください。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 扇町ボンバーズ
+                          v-expansion-panel-content
+                            | 北海道札幌を中心に活動しているバンドです。年齢、出身地、出身校、仕事も全く異なる社会人6名が、それぞれの好みやこだわりをぶつけ合い試行錯誤の日々。今年、参加予定だった旭川ブルーグラスフェスティバルが中止になってしまったので、はじめての動画配信にチャレンジしてみました。不慣れなところも多々ありますが、楽しんでいただければ幸いです。この動画は、札幌市文化芸術公演配信補助金「さっぽろアートライブ」という企画のために制作したもので、そちらの12曲フルバージョンは12月30日からYouTube上にて公開予定です。12月30日以降に「さっぽろアートライブ、扇町ボンバーズ」で検索して是非ご視聴ください。 撮影場所：珈琲・軽食ひいらぎ（札幌市中央区）会場の”ひいらぎ”は旭川ブルーグラスフェスティバルの主催・運営のお手伝いをしています。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 米屋 with 原宿限界タピオカボーイズ
+                          v-expansion-panel-content
+                            | 概念空間に舞い降りた究極の爽やかボーイズバンド。長く愛され，様々な形態変化を見せていったブルーグラスの「あの」名曲を我々なりの爽やかでバウンシーかつソリッドなアレンジでお届けします。お嫁さん募集中です。ナァ…どうャ…？
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | ゴールデンロッドガールズ
+                          v-expansion-panel-content
+                            | 結成3年目のゆるふわガールズバンドです。聞いて楽しい&弾いて楽しい演奏を目指して、トラッド曲からポップな曲まで幅広く挑戦中です！がんばるぞ〜〜💪💪
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | ゆうと
+                          v-expansion-panel-content
+                            | 1曲だけですが箸休めに…
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 21:00 - 21:15
+                    v-card-text
+                      |  中入り（休憩） Cluster バーチャルイベント会場切り替えのため
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 21:15 - 22:30
+                    v-card-text
+                      v-expansion-panels
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Unripened Stringplunkers
+                          v-expansion-panel-content
+                            | 結成10年ぐらいになるものの、『完熟することはない・いつまでも成長し続ける』という意味を込めて“Unripened Stringplunkers=まだ熟していない弦楽器弾きたち”笑
+                            | ブルーグラスを破壊…ではなく、ブルーグラスを主軸として、様々なジャンル・空気感にチャレンジ中。
+                            | 今回の Lockdown Fes への準備中、F2F 会議・練習をしないことにより幾度となく崩壊しかけましたが、映像と音声をゴリゴリに編集し無理くり完成させました。
+                            | 個性の集大成をお楽しみいただければと思います。
+                            | イカれたメンバをよろしくな!
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | B-RISE
+                          v-expansion-panel-content
+                            | B-rise です。 なかなか室内で集まっての練習も憚られる日々、練習はもっぱら近所の公園。 憧れの名曲”Yankees revenge “を目指すも、撮影したものを見てみると、元曲にみなぎる殺気が感じられない､､､と言う事で”calm revenge “”穏やかな復讐”と言う題にしました。 最後まで聞いて頂ければ嬉しいです。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 日本アルプス弦楽団
+                          v-expansion-panel-content
+                            | B-rise です。 なかなか室内で集まっての練習も憚られる日々、練習はもっぱら近所の公園。 憧れの名曲”Yankees revenge “を目指すも、撮影したものを見てみると、元曲にみなぎる殺気が感じられない､､､と言う事で”calm revenge “”穏やかな復讐”と言う題にしました。 最後まで聞いて頂ければ嬉しいです。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 箸休め?Sacred
+                          v-expansion-panel-content
+                            | 「ブルーグラスの背後にあるキリスト教の文脈の把握」
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Copper Kettles
+                          v-expansion-panel-content
+                            | メンバーのほとんどは北大ブルーグラス研究会のOBです。Country Gentlemen の曲を中心に、他の曲もやるバンドです。18年と19年のフェスの動画をエントリーします。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | まいばす
+                          v-expansion-panel-content
+                            | 非日常の中で感じる日常と、日常の中で演出した非日常と、それらを内包して回っていく個人的生活について。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | れいじーぴっかーず
+                          v-expansion-panel-content
+                            | いつもお世話になっております。れいじーぴっかーずです。 前回はライブの動画でしたので今回はリモートで演奏を重ねてみました。 オリジナルのインスト曲で「五条別れ」といいます。 ドブロの池田が作曲しました。曲名は曲のモチーフを思いついた場所の地名から取りました。 短いですが結構な難曲です。頑張って演奏しましたのでみなさんに聴いていただきたいです。 どうぞよろしくお願いします。
+                v-timeline-item
+                  v-card
+                    v-card-title
+                      | 22:30 - 24:00
+                    v-card-text
+                      v-expansion-panels
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 鳩正宗
+                          v-expansion-panel-content
+                            | 2009年結成。神戸を中心に活動中。Traditionalな曲から色物まで、いろんな曲やってます！New Grass Revivalが多め。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 雑貨屋フレイヴァー
+                          v-expansion-panel-content
+                            | 2007年、東北大ブルーグラス同好会の部室に居合わせた面々で結成。今ではそれぞれ社会人となり、家業の都合やメンバーの海外転勤などにより活動が少なくなっていましたが、今回リモートセッションで距離、時差の壁を越えて参加します。Lockdown fesのために新曲2曲を書き下ろしてきました！
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | マーマレードボーイズ
+                          v-expansion-panel-content
+                            | みなさま 初めまして、大谷大学OBのマッキーです。 大谷大学OBのマッキーとひらりんが1年の時、「いつかツインギターのバンドを絶対組もう！」と言っていたが、 誰も参加する人がおらず、 残ったのはギター２つと野郎が２人、あとOOOが２本、、、、、、、、、、、、、、、 ギター初心者だった二人が切磋琢磨など一切せず！互いに好きなように楽しんだ３年間の一興として、楽しんでもらえれば幸いです。 野郎２人が自分達なりに、無い頭絞ったんで、生温かい目でみてもらえれば恐縮です。 それではお聞きください、「マーマレードメドレー♪」 マッキー
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | コーラスウォーター
+                          v-expansion-panel-content
+                            | みなさま 初めまして、大谷大学OBのマッキーです。 大谷大学OBのマッキーとひらりんが1年の時、「いつかツインギターのバンドを絶対組もう！」と言っていたが、 誰も参加する人がおらず、 残ったのはギター２つと野郎が２人、あとOOOが２本、、、、、、、、、、、、、、、 ギター初心者だった二人が切磋琢磨など一切せず！互いに好きなように楽しんだ３年間の一興として、楽しんでもらえれば幸いです。 野郎２人が自分達なりに、無い頭絞ったんで、生温かい目でみてもらえれば恐縮です。 それではお聞きください、「マーマレードメドレー♪」 マッキー
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | MTB
+                          v-expansion-panel-content
+                            | MTB(Master of Traditional Bluegrass )を目指して活動を始めました！ fd テイター(安田) bj もっつ md アッキー gt 村田
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | Deep Blue Boys
+                          v-expansion-panel-content
+                            | 歌、リズム、ハーモニー、そして楽器、全てがそこそこの出来で調和できたそこそこのバンドです。お楽しみください。
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | ジャンクション
+                          v-expansion-panel-content
+                            | ブルーグラスに用いる楽器でサカナクションのアレンジを試みる実験的バンド お馴染みの楽器から出される音色で、表拍に強調を置いたテクノ・ダンスサウンドに挑戦します サウンドエンジニアリングガチ勢によるミックスは必聴です
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | かみおか（おだ）
+                          v-expansion-panel-content
+                            | こんばんは。北大ブルーグラスOBのおだです。前回のフェスではかみおかwithぴぐもんとしてデュエットで参加しましたが、今回は1人で2曲やります。前回のオンラインフェスは顔の見えない動画が多かったので、顔芸で勝負します！バンドの方は、仲が悪くなって解散した…とかじゃありません。むしろ結婚しました。お祝い投票求む！！！！！！！
+                        v-expansion-panel
+                          v-expansion-panel-header
+                            | 水木ゆうこ＆サウザンドリバー
+                          v-expansion-panel-content
+                            | 2020年第二回オンラインフェス参加用に再編集！H・M・フォーク村「小さな仲間フォーク・コンサート」水木ゆう子＆サウザンドリバーライブ演奏。ボーカル＆ギター：畠山和治、ハーモニー＆ベース：野口冨次郎、バンジョー：石切山平仁。尚、水木ゆうこ、荒井信夫、小島慎司は所用でお休み。演奏は三十数年前のものです。よろしくお願いします。
+
     v-row
       v-col(cols=12)
         v-card(color='rgb(100, 100, 100, 0.4)')
