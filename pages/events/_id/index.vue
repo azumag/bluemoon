18fdcf785e2653180b0328637869b90bd3c9b3a0
azumag <tsubasa.azumagakito@gmail.com>
2020-12-23 12:01:41 +0900

    update ind

--- pages/events/_id/index.vue
+++ pages/events/_id/index.vue
@@ -119,12 +119,13 @@ v-layout(column, justify-center, align-center)
       v-col(cols=12)
           v-card(color='rgb(100, 100, 100, 0.4)')
             v-card-title.headline.red--text
-              | 放送終了いたしました。本当にありがとうございました。投票ページをオープンしておりますので、是非各エントリに対しての投票お願いします
-            v-btn(@click="gotoVote()"
-              outlined
-              block
-            )
-              | 投票ページ
+              | 放送終了いたしました。本当にありがとうございました。
+            //-   投票ページをオープンしておりますので、是非各エントリに対しての投票お願いします
+            //- v-btn(@click="gotoVote()"
+            //-   outlined
+            //-   block
+            //- )
+            //-   | 投票ページ
     v-row
       v-col(cols=12)
           v-card(color='rgb(100, 100, 100, 0.4)')
