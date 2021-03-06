6f8b2b4f1d02963c93d9bfcaf549b92c42a434d5
azumag <tsubasa.azumagakito@gmail.com>
2021-03-07 01:07:32 +0900

    add tts page for tokens

---
+++ pages/twitchtts.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div
+   | YOUR ACCESS TOKEN:
+   div.title
+    | {{ token }}
+</template>
+
+<script>
+export default {
+  layout: 'util',
+  data() {
+    return {
+      user: null,
+    }
+  },
+  computed: {
+    token() {
+      const endIndex = this.$route.hash.indexOf('&')
+      return this.$route.hash.substring(14, endIndex)
+    },
+  },
+}
+</script>
