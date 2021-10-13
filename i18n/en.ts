aca1ce07b9942a25a654292844d0d73aba7777b9
tempakyousuke <tempakyousuke@gmail.com>
2021-10-13 23:29:01 +0900

    configure i18n

---
+++ i18n/en.ts
@@ -0,0 +1,103 @@
+export const enMessages = {
+  welcome: 'Welcome',
+  notice: 'Notice',
+  ofuse: 'To give money',
+  superLike: 'Super Like Award',
+  awardTitle: 'Award title',
+  bestPerfomance: 'Best Performance Award',
+  emotional: 'Emotional Award',
+  specialAward: 'Special Award',
+  noticeCraftedAward:
+    'For each crafted video entry, ask for the following votes, and the one with the highest number of votes will be the prize.',
+  prize: 'Prize',
+  repairTicket: 'Iwashita Mandolins & Guitars Repair Ticket (Re-FLET)',
+  liveVideoAward: 'Live Video Award',
+  liveVideoAwardPrize: 'Prize: Bluemoon Beer',
+  craftedVideoAward: 'Crafted Video Award',
+  descLiveVideoAward:
+    'The most popular entry out of the live video entries will be chosen in the voting.',
+  QAPrepareVideo: 'Should we make an entry after preparing the movie?',
+  QAPrepareVideoAnswer: 'Entry first, Upload second  is acceptable.',
+  QAVideoTooShort: 'The movie is too short, would it be acceptable?',
+  QAVideoTooShortAnswer:
+    'Of course yes! We are very glad for you to make an entry even one song, or one minute.',
+  QAVideoKind: 'What kind of video is accepted?',
+  QAVideoKindAnswer:
+    'There is no strict rule. Basically, we will accept whatever the player claims “Bluegrass”. Family music such as Irish music, old time music will also be welcomed.',
+  QAPastVideo: 'Is that OK to entry with already published videos?',
+  QAPastVideoAnswer:
+    'Absolutelly Yes except for entried videos in past festival.',
+  okashi: 'Macaron & Baked Sweets from the patisserie Hirasawa',
+  creative: 'Creative & Originality Award',
+  pick: 'Iwashita Mandolins & Guitars Original Wooden pick',
+  descriptionAward:
+    'Conduct a popularity contest for each entry. The voting page will be opened after the festival broadcast. The voting period will last three days, and you can vote for your most memorable entry from a variety of prizes. The first place winner of each award will receive a prize. In the event of a tie, we will draw for the prize. There is only one prize per entry and multiple prizes will be applied in a drawing.',
+  noticeAward:
+    'Each prize is limited to one piece. Even if your band is made up of five members, only one prize will be delivered.',
+  gotoOfuse: 'Link to give money',
+  QAhowtoMakeVideos: 'How to make entry videos?',
+  QAhowtoMakeVideosAnswer: '',
+  aboutTimetable: 'About the timetable',
+  howtoEntry: 'How to entry',
+  events: 'Events',
+  komeyaAward: 'Komeya Award',
+  descKomeyaAward:
+    'The criteria is a secret... and well see what we can come up with!',
+  komeyaAwardPrize: 'Some of canned shōchū highballs',
+  openEvent: 'Opening Event',
+  eventDescription: 'Information of events and registrations',
+  ja: 'Japanese',
+  filmEditing: 'Film Editing Award',
+  appleJuice: 'Apple Juice',
+  scheduledEvent: 'Scheduled Events',
+  finishedEvent: 'Finished Events',
+  entries: 'Your own entries',
+  openRegistration: 'Registration has been started',
+  registrationNotice: 'Please login for registration',
+  musicTech: 'Music Mastering / Mixing Award',
+  jindaijiBeer: 'Jindaiji Beer',
+  registrationNotice2: '',
+  bluegrassLove: 'Bluegrass Love Award',
+  entryForm: 'Entry Form',
+  entryDescription:
+    "You can edit/withdraw your entries after submission. It's available to multiple entries, however, notße that please avoid same band entry.",
+  formNameHint: '(e.g., Band name)',
+  formEntryLabel: 'Entry Name',
+  formEmailHint: '* information/confirmation may be sent to you.',
+  formEmailLabel: 'Your email address',
+  formDescriptionLabel: 'Description of videos or your band.',
+  formDescriptionHint:
+    "(e.g., A bluegrass band formed in 2010. Centered on the guitar and vocals born from the leader's unique sense of rhythm, Characterized by an aggressive and bouncy sound that makes you can't help but move your body.)",
+  formVideoTypeLabel: 'Registration Type',
+  formVideoTypeCraftedLabel:
+    'The video taken for this event: approx. 15minutes of less',
+  formVideoTypeLiveLabel:
+    'The video of past live performance: approx. 15minutes of less',
+  formVideoTypeMixedLabel: 'Mixed: approx. 10',
+  formVideoTypeNotice:
+    "Even if you don't have a video yet, you can upload it until the deadline",
+  soba: 'Jindaiji Soba & Special Pick',
+  formFileURLLabel:
+    'URL for video (can be more than one if the total time is in a limitation)',
+  formUploadLabel:
+    'Upload your video with form (available multiple uploadings)',
+  formPublishAgreeLabel:
+    'Whether or not allowing us to publish the video for archive',
+  formPublishAgreeNotice:
+    'If you allow archival publishing, it may be included in the videos that will be published as "festival footage" after the online festival',
+  submit: 'Submit',
+  registrationFinished: 'Thank you for your entry!',
+  fileUploadError: 'File Upload Error',
+  entryList: 'Your own entries',
+  edit: 'Edit',
+  withdraw: 'Withdraw',
+  required: 'Requied',
+  uploadedFiles: 'Uploaded Files',
+  delete: 'Delete',
+  update: 'UPDATE',
+  deleteConfirmation: 'Cancel the entry, OK?',
+  deleted: 'Withdrawed',
+  deleteError: 'Error',
+  updateEntry: 'Entry Updated',
+  detail: 'Detail',
+};
