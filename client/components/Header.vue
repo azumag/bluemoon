100ae24e9e4c0934578df7ec4dea521f92ff19d0
tempakyousuke <tempakyousuke@gmail.com>
2021-10-13 19:11:45 +0900

    create header component

---
+++ client/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue'
+
+type Props = {
+  title: String;
+};
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+  },
+})
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="bg-black text-white h-screen">
+    <Header title="bluemoon" />
     <slot />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+  },
+})
+</script>
