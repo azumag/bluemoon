e1085eb22678855715c72ccb74433ec1a8ecad75
tempakyousuke <tempakyousuke@gmail.com>
2021-10-14 10:45:09 +0900

    create NavLink component

---
+++ client/components/Navigation/NavLink.vue
@@ -0,0 +1,42 @@
+<template>
+  <nuxt-link :to="item.to">
+    <div
+      class="link"
+      :class="{
+        'route-match': routeMatch($route.path),
+      }"
+      @click="$emit('close')"
+    >
+      {{ item.i18n ? $t(item.i18n) : item.title }}
+    </div>
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+interface Nav {
+  icon: string;
+  title?: string;
+  i18n?: string;
+  to: string;
+}
+
+const props = defineProps<{
+  item: Nav;
+}>();
+
+defineEmits<{ (e: 'close'): void }>();
+
+const routeMatch = (path: string) => {
+  return props.item.to === path;
+};
+</script>
+
+<style scoped>
+.link {
+  @apply p-2 text-lg font-semibold border-gray-100 hover:bg-gray-700;
+}
+
+.route-match {
+  @apply bg-gray-600;
+}
+</style>
