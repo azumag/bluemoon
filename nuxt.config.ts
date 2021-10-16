d19b96f971d042643af6de0d205880c841744b12
tempakyousuke <tempakyousuke@gmail.com>
2021-10-16 19:43:03 +0900

    remove dotenv module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,4 @@ import { defineNuxtConfig } from 'nuxt3';
 export default defineNuxtConfig({
   ssr: false,
   rootDir: './client',
-  modules: [
-    '@nuxtjs/dotenv',
-    {
-      filename:
-        process.env.NODE_ENV !== 'production'
-          ? './config/.env.dev'
-          : './config/.env.prod',
-    },
-  ],
 });
