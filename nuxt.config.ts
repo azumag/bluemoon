0d684b3e3fcc8b189726337b355ea942435cac9a
tempakyousuke <tempakyousuke@gmail.com>
2021-10-16 18:31:54 +0900

    set dotenv config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@ import { defineNuxtConfig } from 'nuxt3';
 export default defineNuxtConfig({
   ssr: false,
   rootDir: './client',
+  modules: [
+    '@nuxtjs/dotenv',
+    {
+      filename:
+        process.env.NODE_ENV !== 'production'
+          ? './config/.env.dev'
+          : './config/.env.prod',
+    },
+  ],
 });
