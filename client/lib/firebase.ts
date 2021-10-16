e27f3d7af678a64d3cfb0614139253d63724fcb5
tempakyousuke <tempakyousuke@gmail.com>
2021-10-16 19:22:47 +0900

    prepare firebase module

---
+++ client/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+initializeApp({
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+});
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
