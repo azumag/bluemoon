0d1366ae3926f728b8359248f75e870b6353fc02
tempakyousuke <tempakyousuke@gmail.com>
2021-11-01 17:24:51 +0900

    create Button component

---
+++ client/components/Button/BaseButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    class="button"
+    :class="buttonClass"
+    type="button"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  bgColor: string;
+  bgOpacity: string;
+  hoverBgOpacity: string;
+  disabled: boolean;
+  block: boolean;
+}
+
+const props = defineProps<Props>();
+const buttonClass = computed(() => {
+  const buttonClass: {
+    [key: string]: boolean;
+  } = {
+    disabled: props.disabled,
+  };
+  buttonClass[props.bgColor] = true;
+  buttonClass[props.bgOpacity] = true;
+  buttonClass[props.hoverBgOpacity] = !props.disabled;
+  if (props.block) {
+    buttonClass['w-full'] = true;
+  }
+  return buttonClass;
+});
+
+defineEmits<{
+  (e: 'click'): void;
+}>();
+</script>
+
+<style scoped>
+.button {
+  @apply py-1 px-3 rounded;
+}
+.disabled {
+  opacity: 0.7;
+}
+</style>
---
+++ client/components/Button/Button.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="inline-block" :class="{ 'w-full': block }">
+    <nuxt-link v-if="to !== ''" :to="to" :target="target">
+      <BaseButton
+        :bg-color="bgColor"
+        :bgOpacity="bgOpacity"
+        :hoverBgOpacity="hoverBgOpacity"
+        :disabled="disabled"
+        :block="block"
+        @click="$emit('click')"
+      >
+        <slot />
+      </BaseButton>
+    </nuxt-link>
+    <a v-else-if="href !== ''" :href="href" :target="target">
+      <BaseButton
+        :bg-color="bgColor"
+        :bgOpacity="bgOpacity"
+        :hoverBgOpacity="hoverBgOpacity"
+        :disabled="disabled"
+        :block="block"
+        @click="$emit('click')"
+      >
+        <slot />
+      </BaseButton>
+    </a>
+    <BaseButton
+      v-else
+      :bg-color="bgColor"
+      :bgOpacity="bgOpacity"
+      :hoverBgOpacity="hoverBgOpacity"
+      :disabled="disabled"
+      :block="block"
+      @click="$emit('click')"
+    >
+      <slot />
+    </BaseButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import BaseButton from './BaseButton.vue';
+interface Props {
+  to?: string;
+  href?: string;
+  target?: string;
+  bgColor?: string;
+  bgOpacity?: string;
+  hoverBgOpacity?: string;
+  disabled?: boolean;
+  block?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  to: '',
+  href: '',
+  target: '',
+  bgColor: 'bg-blue-500',
+  bgOpacity: 'bg-opacity-100',
+  hoverBgOpacity: 'hover:bg-opacity-80',
+  disabled: false,
+  block: false,
+});
+
+defineEmits<{
+  (e: 'click'): void;
+}>();
+</script>
