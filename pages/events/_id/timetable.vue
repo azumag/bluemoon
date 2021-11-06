86e069780f98d2c96e1a09dee704dd9cf3b58380
azumag <tsubasa.azumagakito@gmail.com>
2021-11-06 23:16:40 +0900

    clean repo

---
+++ pages/events/_id/timetable.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+v-layout(column, justify-center, align-center)
+  v-flex(xs12, sm12, md12, lg12)
+    v-row
+      v-col(cols=12)
+        v-card(color='rgb(100, 100, 100, 0.4)')
+          v-img(
+            class="white--text align-end"
+            height="200px"
+            src="/fes2-sm.jpg"
+          )
+            v-card-title.headline
+              | Festival Timetable
+</template>
+<script>
+export default {
+  data() {
+    return {
+      question: '',
+    }
+  },
+  methods: {},
+}
+</script>
