d9c3975d7c478e0a91d5a8ecf9a5a97985ba6e4c
azumag <tsubasa.azumagakito@gmail.com>
2024-04-14 21:31:45 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10.16.2-alpine
+FROM node:12-alpine
 ENV LANG C.UTF-8
 
 WORKDIR /app/bluemoon
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./static/**/*.{html,js,jsx,ts,tsx}", // すべてのHTMLとJavaScript/TypeScriptファイルを含める
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
