fb6574fde7f49ee7c30a6591432e29cf4492fc77
Tsubasa Azumagakito <azumag@users.noreply.github.com>
2026-08-06 04:45:48 +0900

    [Epic2] Cloudflare Workers Static Assetsへのデプロイ設定と本番切替 (#279)
    Closes #263\nCloses #264

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://bluemoon-82c0b.web.app',
+  site: 'https://www.bluemoon.works',
   trailingSlash: 'ignore',
   integrations: [mdx()],
 });
